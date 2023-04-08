aebdf79ca814c24ad6e7232ea973823624c71d70
ChihyuenLi <524975199@qq.com>
2023-04-09 00:34:20 +0800

    fix:avatarurl

--- miniprogram/pages/login/login.ts
+++ miniprogram/pages/login/login.ts
@@ -4,70 +4,73 @@ const app = getApp()
 
 Page({
   data: {
-    school_list:[],
-    avatarUrl:"../../icon/touxiang.png",
-    nickname:"微信名",
+    school_list: [],
+    avatarUrl: "../../icon/touxiang.png",
+
+    nickname: "微信名",
     hasUserInfo: false,
     array: ['选项一', '选项二', '选项三'],
     index: 0,
-    birthdate:"与身份证相同",
-    school:"学校"
+    birthdate: "与身份证相同",
+    school: "学校"
   },
-  onLoad(e:any){
+  onLoad(e: any) {
     const db = wx.cloud.database()
     db.collection('school_database').get({
-      success: res=> {
-      console.log("success",res.data);
-      const schoolList = res.data.map(item=> item.school_name); // 获取 school_name 字段组成的数组
+      success: res => {
+        console.log("success", res.data);
+        const schoolList = res.data.map(item => item.school_name); // 获取 school_name 字段组成的数组
 
-      this.setData({
-        school_list: schoolList
-      });
-     },
-     fail(err) {
-       console.error(err);
-     }
-    })    
+        this.setData({
+          school_list: schoolList
+        });
+      },
+      fail(err) {
+        console.error(err);
+      }
+    })
   },
-  onChange: function(e:any) {
+  onChange: function (e: any) {
     console.log('选中的值为：', e.detail.value)
     this.setData({
       index: e.detail.value
     })
   },
-  bindSchoolPickerChange (e){
+  bindSchoolPickerChange(e) {
     const index = e.detail.value;
     const value = this.data.school_list[index];
     this.setData({
       school: value
     });
   },
-  bindDateChange: function(e:any) {
+  bindDateChange: function (e: any) {
     this.setData({
-      birthdate:e.detail.value,
+      birthdate: e.detail.value,
     })
   },
-  //用户选中自定义头像的回调
   onChooseAvatar(e) {
-    const { avatarUrl } = e.detail  //获取图片临时路径
+    const {
+      avatarUrl
+    } = e.detail
+    console.log(avatarUrl);
     this.setData({
-      avatarUrl,
+      avatarUrl: avatarUrl,
     })
   },
-  changeNickName(e:any){
+  changeNickName(e: any) {
     this.setData({
-      nickname:e.detail.value,
+      nickname: e.detail.value,
     })
   },
 
-  backToMine(){
-    getApp().globalData.isLogin=true;
-    console.log( getApp().globalData.isLogin)
-      wx.navigateBack({
-        delta:1,
-        success: function(res) {
-           console.log("返回成功")
-        }
-      })
+  backToMine() {
+    getApp().globalData.isLogin = true;
+    console.log(getApp().globalData.isLogin)
+    wx.navigateBack({
+      delta: 1,
+      success: function (res) {
+        console.log("返回成功")
+      }
+    })
   }
 })
